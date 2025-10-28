a2b87012d331d80cb6e0ee475f580839cb15adad
thampe123 <theresa.ampe@ugent.be>
2025-10-28 12:34:33 +0100

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config: 
+    SECRET_KEY = 'Project_group39'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[Project_group39]@db.bsjhjoukbvkycvfgcyzc.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
