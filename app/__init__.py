a2198f4aa2fd3d6701f2750d18119263238fd445
thampe123 <theresa.ampe@ugent.be>
2025-10-31 10:38:07 +0100

    theresa
    code werkt bij mij, van hieruit kunnen we starten en ons eigen project beginnen

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,15 @@
 # app/__init__.py
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from .config import Config
-from .models import db
 
-migrate = Migrate()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-    migrate.init_app(app, db)
-
-    with app.app_context():
-        from .routes import main
-        app.register_blueprint(main)
-
     return app
